a36468e6a917c7d9486243a931fddd51a9b751f4
Velmurugan Manickam <Velmurugan.Manickam@csscorp.com>
2020-06-08 17:30:49 +0530

    lifebuoy HandSanitizer page added

---
+++ Controllers/lifebuoyController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aspnet2Azure.Controllers
+{
+    public class lifebuoyController : Controller
+    {
+        // GET: lifebuoy
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult HandSanitizer()
+        {
+            return View();
+        }
+    }
+}
